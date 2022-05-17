30287f11e5600ef5e59130c6ae79848a0e7b61b2
brijeshkumarrai <brijeshrai9820@gmail.com>
2022-05-17 22:14:43 +0530

    naturalNumber added

--- NaturalNumber.java
+++ NaturalNumber.java
@@ -1,47 +1,15 @@
 package com.firstApp;
 import java.util.Scanner;
-public class NaturalNumber {
 
-  // method to find sum of N natural numbers
-  public static int naturalNumberSum(int number){
-
-     int i = 1; // iterator variable
-     // variable to store sum value
-     int sum = 0;
-
-     // loop to repeat the process
-     while (i<=number) {
-
-        // add into sum value
-        sum = sum + i;
-        // increase iterator variable
-        i++;
-     }
-
-     // return sum value
-     return sum;
-  }
-
-  public static void main(String[] args) {
-
-     // declare variables
-     int number = 0;
-     int sum = 0;
-
-     // create Scanner class object
-     Scanner scan = new Scanner(System.in);
-
-     // read input
-     System.out.print("Enter N value:: ");
-     number = scan.nextInt();
-
-     // Calculate the sum value
-     sum = naturalNumberSum(number);
-
-     // display result
-     System.out.println("Sum = "+sum);
-
-     // close Scanner class objects
-     scan.close();
-  }
+public class NaturalNumber{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number");
+		int number = sc.nextInt();
+		int totalSum = number * (number+1)/2;
+		System.out.println(totalSum);
+		sc.close();
+	}
+	
+	
 }
